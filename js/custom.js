3f1c76578982e21095d63799b15880ac2dbebadf
thangbk7010 <thangbk7010@gmail.com>
2015-05-10 20:43:33 +0700

    update tabs

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,23 @@
+(function($){
+	$.fn.tabs = function(){
+		var tabs = this;
+		
+		if(tabs.length == 0) return tabs;
+		
+		tabs.find('.tab-title li').click(function(event){
+			event.preventDefault();
+			var id = $(this).attr('rel');
+			tabs.find('.tab-title .active').removeClass('active');
+			tabs.find('.tab-content').hide().filter('#' + id).show();
+			$(this).addClass('active');
+		});
+		
+		tabs.find('.tab-content').hide().filter('#' + tabs.find('.tab-title .active').attr('rel')).show();
+		
+		return this;
+	}
+})(jQuery);
+
 $(document).ready(function(){
 	$('.show-more > a').click(function(event){
 		event.preventDefault();
@@ -39,6 +59,7 @@ $(document).ready(function(){
 			scrollTop: 0
 		},1000);
 	});
+	$('.tabs').tabs();
  });
  
  jQuery(window).resize(function(){
